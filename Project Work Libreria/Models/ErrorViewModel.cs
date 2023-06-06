b0fe5b5e599e6006b1da56d9901062daa81f6a93
tpaglia493 <tommasopaglia93@outlook.it>
2023-06-06 09:56:48 +0200

    Aggiungere i file di progetto.

---
+++ Project Work Libreria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_Work_Libreria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
