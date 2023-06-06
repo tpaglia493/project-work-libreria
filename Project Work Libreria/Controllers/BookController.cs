a4c3507b12f46cce701e0505c95de00d0151fa74
thrudr <ariel.oliveiraesilva@gmail.com>
2023-06-06 11:32:21 +0200

    create bookController

---
+++ Project Work Libreria/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Work_Libreria.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
