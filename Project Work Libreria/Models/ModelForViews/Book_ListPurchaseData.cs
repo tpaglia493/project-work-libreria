01aea3f1a6893910d8cb20405b2582977e1c40ef
tpaglia493 <tommasopaglia93@outlook.it>
2023-06-07 11:59:22 +0200

    commit to merge

---
+++ Project Work Libreria/Models/ModelForViews/Book_ListPurchaseData.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_Work_Libreria.Models.ModelForViews
+{
+    public class Book_ListPurchaseData
+    {
+        public Book PurchasedBook { get; set; }
+        public List<PurchaseData> purchaseDatas { get; set; }
+
+
+    }
+}
