587db3453adf82281207bf7bf6297486e8929feb
tpaglia493 <tommasopaglia93@outlook.it>
2023-06-09 10:25:04 +0200

    back to mvc

---
+++ Project Work Libreria/Models/ModelForViews/PurchaseData_ListBooks.cs
@@ -0,0 +1,9 @@
+﻿namespace Project_Work_Libreria.Models.ModelForViews
+{
+    public class PurchaseData_Book
+    {
+        public PurchaseData PurchaseData { get; set; }
+        public Book Book { get; set; }
+
+    }
+}
