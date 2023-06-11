e0c869d34be26b6fd2f3b5839bcf0009a6eb57e8
tpaglia493 <tommasopaglia93@outlook.it>
2023-06-11 18:11:19 +0200

    adding controller api

---
+++ Project Work Libreria/Controllers/ControllersAPI/AdminPurchaseDataApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Work_Libreria.Controllers.ControllersAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminPurchaseDataApiController : ControllerBase
+    {
+    }
+}
