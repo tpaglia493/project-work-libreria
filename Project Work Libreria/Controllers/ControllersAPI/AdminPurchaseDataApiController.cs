96847be3dbf5439d6086af6c7db9cd2ea64b99b6
tpaglia493 <tommasopaglia93@outlook.it>
2023-06-11 18:19:45 +0200

    commented admin view

--- Project Work Libreria/Controllers/ControllersAPI/AdminPurchaseDataApiController.cs
+++ Project Work Libreria/Controllers/ControllersAPI/AdminPurchaseDataApiController.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_Work_Libreria.Database;
+using Project_Work_Libreria.Models;
 
 namespace Project_Work_Libreria.Controllers.ControllersAPI
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class AdminPurchaseDataApiController : ControllerBase
     {
+        [HttpPost]
+        public IActionResult Create([FromBody] AdminPurchaseData AdminPurchaseData)
+        {
+
+            using (BookShopContext db = new BookShopContext())
+            {
+
+                db.AdminPurchaseDatas.Add(AdminPurchaseData);
+                db.SaveChanges();
+
+            }
+
+
+            return CreatedAtAction(nameof(Create), new { id = AdminPurchaseData.Id }, AdminPurchaseData);
+        }
     }
 }
