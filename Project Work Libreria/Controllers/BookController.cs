d2fb7562eb7c29b7fa4d0d345ca004157004e780
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-06-13 15:13:43 +0200

    add index list book

--- Project Work Libreria/Controllers/BookController.cs
+++ Project Work Libreria/Controllers/BookController.cs
@@ -12,33 +12,14 @@ namespace Project_Work_Libreria.Controllers
     {
         //*********************** GET LISTA DI LIBRI CON LA LORO CATEGORIA **************************
 
-        //[Authorize(Roles = "ADMIN,USER")]
-        //public IActionResult Index()
-        //{
-        //    using (BookShopContext db = new())
-        //    {
-        //        //TODO: REFACTORING USANDO .include()
-        //        List<BookCategory> bookCategories = db.Categories.ToList();
-        //        List<Book_ListBookCategories> listOfModels = new();
-        //        foreach (Book book in db.Book)
-        //        {
-        //            Book_ListBookCategories modelForView = new();
-        //            modelForView.Book = book;
-        //            modelForView.BookCategories = bookCategories;
-        //            listOfModels.Add(modelForView);
-
-        //        }
-
-        //        return View("Index", listOfModels);
-        //    }
-        //}
+        
         public IActionResult Index()
         {
             using (BookShopContext db = new BookShopContext())
             {
 
-                List<Book> ourPizza = db.Book.ToList();
-                return View(ourPizza);
+                List<Book> ourBook = db.Book.Include(b => b.Category).ToList();
+                return View(ourBook);
             }
         }
         //******************************* CREARE UN LIBRO ***********************************//
