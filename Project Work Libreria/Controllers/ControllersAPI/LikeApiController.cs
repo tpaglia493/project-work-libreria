ee7b51a8ff6c2e0896ff1545fda88bf2b6d1f4e8
tpaglia493 <tommasopaglia93@outlook.it>
2023-06-15 09:36:28 +0200

    adde user default role on registration

--- Project Work Libreria/Controllers/ControllersAPI/LikeApiController.cs
+++ Project Work Libreria/Controllers/ControllersAPI/LikeApiController.cs
@@ -1,31 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Project_Work_Libreria.Database;
 using Project_Work_Libreria.Models;
+using System.Security.Claims;
 
 namespace Project_Work_Libreria.Controllers.ControllersAPI
 {
-    [Route("api/[controller]/[action]")]
-    [ApiController]
-    public class LikeApiController : ControllerBase
-    {
-        [HttpPost("{id}")]
-        public IActionResult AddLike(int id, [FromBody] Like like)
-        {
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class LikeApiController : ControllerBase
+	{
+		[HttpPost("{id}")]
+		public IActionResult AddLike(int id, [FromBody] Like like)
+		{
 
-            using (BookShopContext db = new BookShopContext())
-            {
-                Book likedBook = db.Book.Where(b => b.Id == id).FirstOrDefault();
+			using (BookShopContext db = new BookShopContext())
+			{
+				Book likedBook = db.Book.Where(b => b.Id == id).FirstOrDefault();
+				string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+				likedBook.UsersThatLiked.Add(userId);
+				likedBook.LikeQuantity += like.Quantity;
+				db.Like.Add(like);
+				db.SaveChanges();
+				return Ok();
+			}
 
-                likedBook.LikeQuantity += like.Quantity;
-                db.Like.Add(like);
-                db.SaveChanges();
-                return Ok();
-            }
 
 
+		}
 
-        }
-
-    }
+	}
 }
 
--- Project Work Libreria/Models/Book.cs
+++ Project Work Libreria/Models/Book.cs
@@ -1,62 +1,64 @@
-﻿
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 
 namespace Project_Work_Libreria.Models
 {
-    public class Book
-    {
-        public int Id { get; set; }
-        [Required]
-        public string ImgSource { get; set; }
-        [Required]
-        [StringLength(100, ErrorMessage = "Il titolo non può essere più lungo di 100 caratteri!")]
-        public string Title { get; set; }
-        [Required]
-        [StringLength(100, ErrorMessage = "Il nome dell'autore non può essere più lungo di 100 caratteri!")]
-        public string Author { get; set; }
-        [Required]
-        [StringLength(800, ErrorMessage = "La descirizione non può essere più lunga di 800 caratteri!")]
+	public class Book
+	{
+		public int Id { get; set; }
+		[Required]
+		public string ImgSource { get; set; }
+		[Required]
+		[StringLength(100, ErrorMessage = "Il titolo non può essere più lungo di 100 caratteri!")]
+		public string Title { get; set; }
+		[Required]
+		[StringLength(100, ErrorMessage = "Il nome dell'autore non può essere più lungo di 100 caratteri!")]
+		public string Author { get; set; }
+		[Required]
+		[StringLength(800, ErrorMessage = "La descirizione non può essere più lunga di 800 caratteri!")]
 
-        public string Description { get; set; }
-        [Required]
-        [Range(0, float.MaxValue)]
-        public float Price { get; set; }
+		public string Description { get; set; }
+		[Required]
+		[Range(0, float.MaxValue)]
+		public float Price { get; set; }
 
-        public int? LikeQuantity { get; set; }
+		public int? LikeQuantity { get; set; }
 
-        //TODO: INSERIRE DATA-VALIDATIONS
-        public float? SupplierPrice { get; set; }
+		////TODO:IMPOSTARE RELAZIONE CON USER
+		//public List<string>? UsersThatLiked { get; set; }
 
-        [Range(0, int.MaxValue)]
-        public int? AvailableCopies { get; set; }
+		//TODO: INSERIRE DATA-VALIDATIONS
+		public float? SupplierPrice { get; set; }
 
-        // IMPOSTAZIONE RELAZIONE 1-N CON CATEGORIA
-        public int? BookCategoryId { get; set; }
-        public BookCategory? Category { get; set; }
+		[Range(0, int.MaxValue)]
+		public int? AvailableCopies { get; set; }
 
-
-
-        // IMPOSTAZIONE RELAZIONE N-1 CON PURCHASEDATA  
-        public List<PurchaseData>? PurchaseDatas { get; set; }
+		// IMPOSTAZIONE RELAZIONE 1-N CON CATEGORIA
+		public int? BookCategoryId { get; set; }
+		public BookCategory? Category { get; set; }
 
 
 
+		// IMPOSTAZIONE RELAZIONE N-1 CON PURCHASEDATA  
+		public List<PurchaseData>? PurchaseDatas { get; set; }
 
-        // IMPOSTAZIONE RELAZIONE N-1 CON ADMINPURCHASEDATA  
-        public List<AdminPurchaseData>? AdminPurchaseDatas { get; set; }
 
-        public Book() { }
 
-        public Book(string imgSource, string title, string author, string description, float price, int? availableCopies)
-        {
 
-            ImgSource = imgSource;
-            Title = title;
-            Author = author;
-            Description = description;
-            Price = price;
-            AvailableCopies = availableCopies;
-        }
-    }
+		// IMPOSTAZIONE RELAZIONE N-1 CON ADMINPURCHASEDATA  
+		public List<AdminPurchaseData>? AdminPurchaseDatas { get; set; }
+
+		public Book() { }
+
+		public Book(string imgSource, string title, string author, string description, float price, int? availableCopies)
+		{
+
+			ImgSource = imgSource;
+			Title = title;
+			Author = author;
+			Description = description;
+			Price = price;
+			AvailableCopies = availableCopies;
+		}
+	}
 }
